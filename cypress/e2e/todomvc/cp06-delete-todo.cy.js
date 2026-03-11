a15a8b956f410886e9bcef1dbb3948a12f61e87d
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-03-11 19:39:20 -0300

    test(todomvc): add CP-06 todo delete tests

---
+++ cypress/e2e/todomvc/cp06-delete-todo.cy.js
@@ -0,0 +1,48 @@
+let tasks = []
+
+describe("Delete todo", () => {
+    beforeEach(() => {
+        cy.visit("https://todomvc.com/examples/react/dist/#/active")
+        
+        // Create a task
+        tasks = ["Comprar tomates", "Comprar cebollas", "Comprar pimientos"]
+        tasks.forEach((task) => {
+            cy.addTodo(task)
+        })
+        cy.get(".filters").contains("All").click()
+    })
+
+    it("CP-06-01 - Delete a todo item", () => {
+        // Delete the task
+        cy.get(".todo-list li")
+        .first()
+        .find(".destroy")
+        .click({ force: true })
+
+        cy.get(".todo-list li").should("have.length", 2)
+    })
+
+    it("CP-06-02 - Delete a specific todo item", () => {
+        // Delete a specific task
+        cy.contains("Comprar cebollas")
+        .parents("li")
+        .find(".destroy")
+        .click({ force: true })
+        
+        // Verify the specific task is deleted
+        cy.get(".todo-list li").should("have.length", 2)
+        cy.get(".todo-list li").should("not.contain", "Comprar cebollas")
+    })
+
+    it("CP-06-03 - Delete the last todo item", () => {
+        // Delete the last task
+        cy.get(".todo-list li")
+        .last()
+        .find(".destroy")
+        .click({ force: true })
+        
+        // Verify the last task is deleted
+        cy.get(".todo-list li").should("have.length", tasks.length - 1)
+        cy.contains(`${tasks[tasks.length - 1]}`).should("not.exist")
+    })
+})
