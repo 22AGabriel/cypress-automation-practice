fcd585fe746b1b440820e3b1b2e5f42eb0ef849d
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-03-14 21:46:46 -0300

    test(api): add CP-04 update todo endpoint test

---
+++ cypress/e2e/api/cp04-updateTodo.cy.js
@@ -0,0 +1,43 @@
+describe('CP04 - PUT update todo', () => {
+
+    it('should update a todo and return the updated object', () => {
+        const todoId = 1
+        const endpoint = `https://jsonplaceholder.typicode.com/todos/${todoId}`
+
+        const updatedTodo = {
+            id: todoId,
+            userId: 1,
+            title: 'Updated todo from Cypress API testing',
+            completed: true
+        }
+
+        cy.request({
+            method: 'PUT',
+            url: endpoint,
+            body: updatedTodo
+        })
+        .then((response) => {
+            expect(response.status).to.eq(200)
+
+            expect(response.body).to.be.an('object')
+
+            expect(response.body).to.have.property('userId')
+            expect(response.body).to.have.property('id')
+            expect(response.body).to.have.property('title')
+            expect(response.body).to.have.property('completed')
+
+            // Validate id reamins the same
+            expect(response.body.id).to.eq(todoId)
+
+            // Validate returned values match the updated data
+            expect(response.body.title).to.eq(updatedTodo.title)
+            expect(response.body.completed).to.eq(updatedTodo.completed)
+
+            // Validate data types
+            expect(response.body.userId).to.be.a('number')
+            expect(response.body.id).to.be.a('number')
+            expect(response.body.title).to.be.a('string')
+            expect(response.body.completed).to.be.a('boolean')
+        })
+    })
+})
