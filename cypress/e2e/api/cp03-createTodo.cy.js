0f4fed2250f703ae85b4cca15a98538a69d070ff
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-03-14 21:25:31 -0300

    test(api): add CP-03 create a todo endpoint test

---
+++ cypress/e2e/api/cp03-createTodo.cy.js
@@ -0,0 +1,46 @@
+describe('CP03 - POST create todo', () => {
+
+    it('should create a new todo and return the created object', () => {
+        const endpoint = 'https://jsonplaceholder.typicode.com/todos'
+
+        const newTodo = {
+            userId: 1,
+            title: 'New todo from Cypress API testing',
+            completed: false
+        }
+
+        cy.request({
+            method: 'POST',
+            url: endpoint,
+            body: newTodo
+        })
+        .then((response) => {
+            // Status code should be 201
+            expect(response.status).to.eq(201)
+
+            // Response body should be an object
+            expect(response.body).to.be.an('object')
+
+            // Validate structure
+            expect(response.body).to.have.property('userId')
+            expect(response.body).to.have.property('id')
+            expect(response.body).to.have.property('title')
+            expect(response.body).to.have.property('completed')
+
+            // Validate returned data
+            expect(response.body.userId).to.eq(newTodo.userId)
+            expect(response.body.title).to.eq(newTodo.title)
+            expect(response.body.completed).to.eq(newTodo.completed)
+
+            // Validate data types
+            expect(response.body.userId).to.be.a('number')
+            expect(response.body.id).to.be.a('number')
+            expect(response.body.title).to.be.a('string')
+            expect(response.body.completed).to.be.a('boolean')
+
+            // Validate id generated
+            expect(response.body.id).to.be.greaterThan(0)
+        })
+    })
+
+})
