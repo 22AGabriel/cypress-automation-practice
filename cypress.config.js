9e2ef64dc437521431108d1b0e59ae64dcc83a40
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-03-18 15:44:05 -0300

    test: add mochawesome reporter configuration

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,19 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "https://jsonplaceholder.typicode.com/",
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions:{
+      reportDir: 'cypress/reports',
+      charts: true,
+      reportPageTitle: 'Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true
+    },
+    excludeSpecPattern: ["**/practice/**"],
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
+      return config
     },
   },
 });
