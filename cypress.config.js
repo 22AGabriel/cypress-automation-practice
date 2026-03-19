16362e0f4bae3e40a29edb1d225966f1459b93f2
Gabriel Azubel <gabrielazubel63t@gmail.com>
2026-03-19 19:51:48 -0300

    add reporting setup, failing test examples and documentation screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  video: true,
+  videoUploadOnPasses: false,
+  screenshotOnRunFailure: true,
+
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions:{
+    reportDir: 'cypress/reports/mochawesome',
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+
   e2e: {
     baseUrl: "https://jsonplaceholder.typicode.com/",
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions:{
-      reportDir: 'cypress/reports',
-      charts: true,
-      reportPageTitle: 'Test Report',
-      embeddedScreenshots: true,
-      inlineAssets: true
-    },
     excludeSpecPattern: ["**/practice/**"],
     setupNodeEvents(on, config) {
       // implement node event listeners here
